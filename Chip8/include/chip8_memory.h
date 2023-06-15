1f5c65fdca4c0e6cc90a78a460edfc422cad7b57
Derwin <derwinprince61@gmail.com>
2023-06-14 22:03:30 -0700

    changes

--- Chip8/include/chip8_memory.h
+++ Chip8/include/chip8_memory.h
@@ -9,6 +9,7 @@ struct chip8_memory
     unsigned char memory[CHIP8_MEMORY_SIZE];
 };
 
+// fucntions for talking with memory, setting memory, etc
 void chip8_memory_set(struct chip8_memory* memory, int index, unsigned char val);
 unsigned char chip8_memory_get(struct chip8_memory* memory, int index);
 
--- Chip8/src/main.c
+++ Chip8/src/main.c
@@ -2,12 +2,23 @@
 #include "SDL2/SDL.h"
 #include "chip8.h"
 
+
 int main(int argc, char** argv){
 
+    // declare variable of type struct chip8
     struct chip8 chip8;
-    chip8_memory_set(&chip8.memory, 0x69, 'A');
-    printf("%c\n", chip8_memory_get(&chip8.memory, 50));
 
+    // Store value in hex slot 15[Fh] of array and print that out
+    chip8.registers.V[0xF] = 25;
+    printf("%i\n", chip8.registers.V[0xF]);
+
+    // Example of setting memory and getting from memory
+    chip8_memory_set(&chip8.memory, 0x69, 'B');
+    // Get from memory location 105 which is equal to hex 0x69
+    printf("%c\n", chip8_memory_get(&chip8.memory, 105));
+
+
+    // init SDL and others vars
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow(
         EMULATOR_WINDOW_TITLE, 
@@ -18,9 +29,8 @@ int main(int argc, char** argv){
         SDL_WINDOW_SHOWN);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
-
+    SDL_Event event;
     while(1){
-        SDL_Event event;
         while ((SDL_PollEvent(&event)))
         {
             if(event.type == SDL_QUIT){
@@ -28,22 +38,24 @@ int main(int argc, char** argv){
             }
         }
         
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear;
-        SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 25, 25, 255);
+        SDL_RenderClear(renderer); // clears entire rendering target
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255); // color for rect
+
+        // create rectangle 
         SDL_Rect r;
-        r.x = 0;
-        r.y = 0;
-        r.w = 40;
-        r.h = 40;
-        //SDL_RenderDrawRect(renderer, &r);
+        r.x = 0; r.y = 0; r.w = 40; r.h = 40;
+
+        // fill rect with current target draw color
         SDL_RenderFillRect(renderer, &r);
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(renderer); // present the renderer
     }
 
-    //printf("Hello World!\n");
-out:    
+out:
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    SDL_Quit();
     
     return 0;
 }
